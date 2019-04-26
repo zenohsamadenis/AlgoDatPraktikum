be8fadb6623033f6dc54f709fa7123326ffa293a
CrixxMadine <50016024+CrixxMadine@users.noreply.github.com>
2019-04-26 14:03:46 +0200

    Erstupload Projektgerüst

---
+++ AlgoDatPraktikum/AlgoDatPraktikum/Hashverfahren.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDatPraktikum
+{
+    abstract class Hashverfahren
+    {
+        //
+    }
+
+
+    class HashTabSepChain : Hashverfahren
+    {
+
+    }
+
+    class HashTabQuadProb : Hashverfahren
+    {
+
+    }
+}
---
+++ AlgoDatPraktikum/AlgoDatPraktikum/IDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDatPraktikum
+{
+    interface IDictionary
+    {
+        bool search(int elem);   // true = gefunden
+
+        bool insert(int elem);   // true = hinzugefügt
+
+        bool delete(int elem);   // true = gelöscht
+
+        void print();            // Einfache Ausgabe der Elemente des Wörterbuchs auf der Console,
+                                 // so dass Inhalt und Struktur daraus eindeutig erkennbar
+                                 // Für die Ausgabe von Bäumen wird das in der Übung behandelte Verfahren verwendet
+    }
+
+    interface IMultiSet : IDictionary { }
+
+    interface ISet : IMultiSet { }
+
+    interface IMultiSetSorted : IDictionary { }
+
+    interface ISetSorted : IMultiSetSorted { }
+}
